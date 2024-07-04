f1baf6d5905a67f9977465bc9144e90a1e2ae390
jos-cabrera <joseacabrera@protonmail.com>
2024-07-04 14:33:25 -0300

    Problem 20

---
+++ Easy/Valid Parentheses.py
@@ -0,0 +1,29 @@
+# Problem 20 - Valid Parentheses
+# https://leetcode.com/problems/valid-parentheses
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+        
+        stack = []
+        for i in range(len(s)):
+            if s[i] == "(" or s[i] == "{" or s[i] == "[":
+                stack.append(s[i])
+            else:
+                if len(stack) == 0:
+                    return False
+
+                if s[i] == ")" and stack[-1] == "(":
+                    stack.pop()
+                elif s[i] == "}" and stack[-1] == "{":
+                    stack.pop()
+                elif s[i] == "]" and stack[-1] == "[":
+                    stack.pop()
+                else:
+                    return False
+
+        if len(stack) != 0:
+            return False
+        
+        return True
